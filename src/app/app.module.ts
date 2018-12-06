d8e28a69d897d1e58ac177bb0301dbb940579fe2
Dan <d.blackmore@ucas.ac.uk>
2018-12-06 14:36:55 +0000

    Up to 6.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { appRoutes } from './../routes';
+import { ToastrService } from './common/toastr.service';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './navigation/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  // imports first (dependencies)
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    NavBarComponent
+  ],
+  // services
+  providers: [EventService, ToastrService],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
